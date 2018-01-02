f6358b32049d705504fe83073f07d2f2bd0e0cc8
admin <admin@10.137.138.160>
2018-01-02 16:53:57 +0900

    chapter02

--- src/ichs/science/chapter02/HelloWorld.java
+++ src/ichs/science/chapter02/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World~~");
 		System.out.println("환영합니다, 여러분!");
-		System.out.println("sysout이라 입력하고 컨트롤  스페이스를 누르세여.");
+		System.out.println("sysout이라 입력하고 컨트롤 스페이스를 누르세여.");
 		System.out.println("더블 쿼테이션 마크를 넣고 글을 입력한 뒤에 F11을 눌러보세요.");
 	}
 
