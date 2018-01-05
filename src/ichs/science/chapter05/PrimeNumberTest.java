b561732d4619fe6d52cd276ef140b3bb0e5bdbce
admin <admin@10.137.138.160>
2018-01-05 16:52:09 +0900

    chapter05

--- src/ichs/science/chapter05/PrimeNumberTest.java
+++ src/ichs/science/chapter05/PrimeNumberTest.java
@@ -1,5 +1,10 @@
 package ichs.science.chapter05;
 
 public class PrimeNumberTest {
-
+	public static void main(String[] args) {
+		int number = 23;
+		for(int i = 2; i<number ; i++) {
+			
+		}
+	}
 }
